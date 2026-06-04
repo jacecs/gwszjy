d043b011d948790b928835b361d0d95fdec065e2
jace_cs <jacecsofoxmail.com>
2026-06-05 00:11:41 +0800

    修改菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1100,7 +1100,7 @@ function bindOverviewData(data) {
   dashData.value = data
 
   if (Array.isArray(data.menus)) {
-    // menus.splice(0, menus.length, ...data.menus)
+    menus.splice(0, menus.length, ...data.menus)
   }
   gltfModels.value = Array.isArray(data.gltfs) ? data.gltfs : (data.gltfModels || [])
 
